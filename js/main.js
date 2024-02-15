b6958252fbd5549528c244711b935a5adeb41ee2
C0oki2 <134459864+C0oki2@users.noreply.github.com>
2024-02-15 17:41:23 +0900

    2/15
    슬라이드 내렸다가 올라갈때 버튼 안 사라지게 해야 함, jonse-house 사라진 거 원인 찾아야 함, 문서작업 해야 함

---
+++ js/main.js
@@ -0,0 +1,69 @@
+
+$(function(){
+    $(".a").click (function(){
+        $(".b").slideToggle();
+        
+    });
+});
+
+
+
+/*슬라이더 부분 자바스크립트*/
+   
+if (window.matchMedia("(max-width: 999px)").matches) { 
+
+    $('.prev').click(function(){
+    $('.slide li:last').prependTo('.slide');
+    $('.slide').css('margin-left','-100%');
+    $('.slide').stop().animate({marginLeft:0},800);});
+  
+    $('.next').click(function(){
+      $('.slide').stop().animate({marginLeft:'-100%'},800, function(){
+        $('.slide li:first').appendTo('.slide');
+        $('.slide').css({marginLeft:0}); });
+     });
+}else {
+$('.prev').click(function(){
+    $('.slide li:last').prependTo('.slide');
+    $('.slide').css('margin-left',-1000);
+    $('.slide').stop().animate({marginLeft:0},800);});
+  
+    $('.next').click(function(){
+      $('.slide').stop().animate({marginLeft:-1000},800, function(){
+        $('.slide li:first').appendTo('.slide');
+        $('.slide').css({marginLeft:0}); });
+     });
+
+}
+
+  
+
+/*이순신 응용 부분*/
+      
+$(function() {
+  $("#article_b > div:first")
+  .click(function() {
+      $("#div1").show();
+      $("#div2").hide();
+      $("#div3").hide();
+         
+  });
+
+
+  $("#article_b > div:eq(1)").click(function() {
+      $("#div2").show();
+      $("#div1").hide();
+      $("#div3").hide();
+  });
+
+
+  $("#article_b > div:last").click(function() {
+      $("#div3").show();
+      $("#div1").hide();
+      $("#div2").hide();
+  });
+
+        
+
+
+});
---
+++ js/sub.js
@@ -0,0 +1,68 @@
+
+$(window).scroll(function(){
+    let st = $(window).scrollTop();
+
+    let foot = $('#wrap7').offset().top - $(window).innerHeight();
+    
+    if (st > foot) {
+        $('#sub_wrap2').hide();
+    } else {
+        $('#sub_wrap2').fadeIn();
+    }
+});
+
+/*nav */
+$(function(){
+    $(".a").click (function(){
+        $(".b").slideToggle();
+        
+    });
+});
+
+/*연회비 */
+$(function(){
+    $("#sub_wrap3 h2").click(
+    function(){
+     $("#sub_wrap3 img").toggleClass("click");
+     
+     $("#sub_wrap3 > div ").stop().slideToggle();
+    });
+});
+
+/*할인 한 눈 슬라이드*/
+$(window).scroll(function(){
+        
+    let ws = $(this).scrollTop();
+
+   let fixWrap = $('#leftWrap').offset().top; 
+
+   let move = ws - fixWrap;  
+
+   let end = $('.content').width() - $(window).innerWidth(); 
+
+   if(ws > fixWrap && ws < fixWrap+end){      
+
+     $('.content').css('transform',`translateX(-${move}px)`);
+
+   }else if(ws >= fixWrap+end) {
+
+     $('.content').css('transform',`translateX(-${end}px)`);
+   }
+
+ });
+
+ /*팝업창*/
+     $(function(){
+
+      $('#content_box > img').click(function(){
+        $('#boxWrap').fadeIn();
+
+      });
+
+      $('#close').click(function(){
+        $('#boxWrap').fadeOut();
+      });
+
+    });
+
+
